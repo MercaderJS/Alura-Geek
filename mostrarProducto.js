b10a991301483ff76d43df04ec2af68a6bdccf57
EnmanuelJS <galvanenmanuel0@gmail.com>
2025-01-06 14:20:12 -0400

    cambiando endpoints para nuevo despliegue

--- mostrarProducto.js
+++ mostrarProducto.js
@@ -23,7 +23,7 @@ const idBoton = botonBorrar.getAttribute('data-boton-id');
 botonBorrar.addEventListener("click", (evento) => {
     evento.preventDefault();//evita que se recargue la pagina al eliminar el producto
     const borrarProducto = async () => {
-        const url = `https://alura-geek-mcmy.onrender.com/products/${idBoton}`; // URL del recurso a eliminar
+        const url = `https://alura-geek-tn2y.onrender.com/products/${idBoton}`; // URL del recurso a eliminar
         
         try {
             const response = await fetch(url, {
